c1e82a4b2c96ef0b66572039dc298c5e2af9bf80
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2016-01-30 15:04:39 -0200

    login from username or email #2
    Signed-off-by: Jefferson Xavier <jeffersonx.xavier@gmail.com>
    Signed-off-by: Álex Mesquita <alex.mesquita06@hotmail.com>

--- app/models/social_framework/user.rb
+++ app/models/social_framework/user.rb
@@ -1,8 +1,28 @@
 module SocialFramework
   class User < ActiveRecord::Base
+    attr_accessor :login
+
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable, :validatable
+
+    def login=(login)
+      @login = login
+    end
+
+    def login
+      @login || self.username || self.email
+    end
+
+    def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+     
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["username = :value OR email = :value", { :value => login }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
   end
 end
--- lib/social_framework/engine.rb
+++ lib/social_framework/engine.rb
@@ -1,11 +1,18 @@
+require 'devise'
+
 module SocialFramework
   class Engine < ::Rails::Engine
     isolate_namespace SocialFramework
 
-    initializer :append_migrations do |app|
+    initializer_files("db/migrate")
+    initializer_files("config/initializer")
+  end
+
+  def initializer_files(path)
+    initializer :append_files do |app|
       unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
+        config.paths[path].expanded.each do |expanded_path|
+          app.config.paths[path] << expanded_path
         end
       end
     end
