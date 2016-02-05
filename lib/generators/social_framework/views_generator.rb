8f5086e9714af318dbd2c13d26676e720a37096b
Álex Mesquita <alex.mesquita06@hotmail.com>
2016-02-05 13:38:26 -0200

    Login with username or email #2
    Signed-off-by: Álex Mesquita <alex.mesquita06@hotmail.com>
    Signed-off-by: Jefferson Xavier <jeffersonx.xavier@gmail.com>

--- lib/generators/social_framework/views_generator.rb
+++ lib/generators/social_framework/views_generator.rb
@@ -10,16 +10,19 @@ module SocialFramework
       desc "Add views to app"
 
       def add_views
+        directory :shared, "app/views/devise/shared"
+
         if options[:views]
-        options[:views].each do |directory|
-          directory directory.to_sym, "app/views/devise/#{directory}"
-        end
+          options[:views].each do |directory|
+            directory directory.to_sym, "app/views/devise/#{directory}"
+          end
         else
           directory :confirmations, "app/views/devise/confirmations"
           directory :passwords, "app/views/devise/passwords"
           directory :registrations, "app/views/devise/registrations"
           directory :sessions, "app/views/devise/sessions"
           directory :unlocks, "app/views/devise/unlocks"
+          directory :mailer, "app/views/devise/mailer"
         end
       end
     end
--- lib/social_framework/engine.rb
+++ lib/social_framework/engine.rb
@@ -4,16 +4,16 @@ module SocialFramework
   class Engine < ::Rails::Engine
     isolate_namespace SocialFramework
 
-    initializer_files("db/migrate")
-    initializer_files("config/initializer")
-  end
-
-  def initializer_files(path)
     initializer :append_files do |app|
       unless app.root.to_s.match root.to_s
-        config.paths[path].expanded.each do |expanded_path|
-          app.config.paths[path] << expanded_path
-        end
+        expanded_path("db/migrate", app)
+        expanded_path("config/initializers", app)
+      end
+    end
+
+    def expanded_path(path, app)
+      config.paths[path].expanded.each do |expanded_path|
+        app.config.paths[path] << expanded_path
       end
     end
   end
