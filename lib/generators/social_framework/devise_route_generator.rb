190f8b08796f864501160111cfa436322c15794c
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2016-02-19 13:48:07 -0200

    configuration to use app devise #8
    Signed-off-by: Jefferson Xavier <jeffersonx.xavier@gmail.com>
    Signed-off-by: Álex Mesquita <alex.mesquita06@hotmail.com>

--- lib/generators/social_framework/devise_route_generator.rb
+++ lib/generators/social_framework/devise_route_generator.rb
@@ -2,11 +2,18 @@ module SocialFramework
   module Generators
     class DeviseRouteGenerator < Rails::Generators::Base
 
+      source_root File.expand_path('../../templates/initializers/', __FILE__)
+
       desc "Add devise routes to app"
+
+      def copy_devise_setup
+        copy_file "devise.rb", "config/initializers/devise.rb"
+      end
 
       def add_devise_routes
       	devise_route = "devise_for :users, class_name: 'SocialFramework::User'," <<
-    	"controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}"
+          "controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}"
+        
         route devise_route
       end
     end
--- lib/social_framework/engine.rb
+++ lib/social_framework/engine.rb
@@ -5,15 +5,8 @@ module SocialFramework
     isolate_namespace SocialFramework
 
     initializer :append_files do |app|
-      unless app.root.to_s.match root.to_s
-        expanded_path("db/migrate", app)
-        expanded_path("config/initializers", app)
-      end
-    end
-
-    def expanded_path(path, app)
-      config.paths[path].expanded.each do |expanded_path|
-        app.config.paths[path] << expanded_path
+      config.paths["db/migrate"].expanded.each do |expanded_path|
+        app.config.paths["db/migrate"] << expanded_path
       end
     end
   end
