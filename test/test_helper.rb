6b89eba84b65df1f7fd057d8ce6df681fac9751b
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2016-02-21 20:14:04 -0300

    adding travis ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,9 @@ ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
 require "rails/test_help"
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
