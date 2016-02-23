8c4d44162f29c29d2aee2191a6754cbf17b28c7b
Álex Mesquita <alex.mesquita06@hotmail.com>
2016-02-23 13:53:24 -0300

    Test of registration controller #1
    Signed-off-by: Álex Mesquita <alex.mesquita06@hotmail.com>
    Signed-off-by: Jefferson Xavier <jeffersonx.xavier@gmail.com>

---
+++ spec/controllers/social_framework/registration_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+module SocialFramework
+  RSpec.describe Users::RegistrationsController, type: :controller do
+  	include Devise::TestHelpers
+    routes {SocialFramework::Engine.routes}
+
+    describe "Permit username attribute" do
+      it "Sign up with username" do
+        request.env["devise.mapping"] = Devise.mappings[:user]
+        post :create, user: {
+          username: "user", email: "user@email.com", password: "password", password_confirmation: "password"
+        }
+        expect(response).to have_http_status(302)
+      end
+
+      it "Account update with username" do
+        request.env["devise.mapping"] = Devise.mappings[:user]
+        user = User.create!(username: "user", email: "user@email.com", password: "password")
+        sign_in user
+        put :update, user: {
+          username: "user1", email: "user1@email.com", current_password: "password"
+        }
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
