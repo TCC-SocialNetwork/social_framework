2d2475a8025d9425ca3d601aaeb2d5fc96731597
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2016-02-26 20:30:42 -0300

    comments to generators

--- lib/generators/social_framework/install_devise_generator.rb
+++ lib/generators/social_framework/install_devise_generator.rb
@@ -1,15 +1,18 @@
 module SocialFramework
   module Generators
+    # Generator to add de principal devise files and configurations
     class InstallDeviseGenerator < Rails::Generators::Base
 
       source_root File.expand_path('../../templates/', __FILE__)
 
       desc "Init devise configurations with devise.rb, routes and principal views"
 
+      # Copy devise.rb file to app, this file is used to define devise configurations
       def copy_devise_setup
         copy_file "initializers/devise.rb", "config/initializers/devise.rb"
       end
 
+      # Add devise routes to app maped to framework controllers
       def add_devise_routes
       	devise_route = "devise_for :users, class_name: 'SocialFramework::User',\n" <<
           "\t\tcontrollers: {sessions: 'users/sessions',\n"<<
@@ -19,6 +22,8 @@ module SocialFramework
         route devise_route
       end
 
+      # Add the principal devise views (registrations and sessions),
+      # whith this it's possible register, update and authenticate users
       def add_views
         directory "views/registrations", "app/views/devise/registrations"
         directory "views/sessions", "app/views/devise/sessions"
--- lib/generators/social_framework/views_generator.rb
+++ lib/generators/social_framework/views_generator.rb
@@ -1,6 +1,13 @@
 module SocialFramework
   module Generators
-
+    # Generator to add devise views, that are: confirmations, passwords, registrations, sessions and unlocks
+    # 
+    # This generator add all views running 'rails generate social_framework:views'
+    # 
+    # Using the '-v' option can add specific views,
+    # ====== Example:
+    # To add registrations and sessions views execute 
+    # 'rails generate social_framework:views -v registrations sessions'
     class ViewsGenerator < Rails::Generators::Base
       class_option :views, aliases: "-v", type: :array,
         desc: "Select specific view directories to generate (confirmations, passwords, registrations, sessions, unlocks, mailer)"
@@ -9,6 +16,10 @@ module SocialFramework
       
       desc "Add views to app"
 
+      # Create devise directory in app/views and add devise views to app
+      # 
+      # Without '-v' option generate all views,
+      # With '-v' option generate specific views.
       def add_views
         directory :shared, "app/views/devise/shared"
 
