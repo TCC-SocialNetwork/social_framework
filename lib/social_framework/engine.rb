b74451c5de7a22cdab06353252a193b6b3640dd3
Álex Mesquita <alex.mesquita06@hotmail.com>
2016-03-01 11:29:07 -0300

    Execute specifics migrations #7
    Signed-off-by: Álex Mesquita <alex.mesquita06@hotmail.com>
    Signed-off-by: Jefferson Xavier <jeffersonx.xavier@gmail.com>

--- lib/social_framework/engine.rb
+++ lib/social_framework/engine.rb
@@ -4,15 +4,29 @@ module SocialFramework
   class Engine < ::Rails::Engine
     isolate_namespace SocialFramework
 
-    initializer :append_files do |app|
-      config.paths["db/migrate"].expanded.each do |expanded_path|
-        app.config.paths["db/migrate"] << expanded_path
+    initializer :append_migrations do |app|
+      path = config.paths["db"].expanded.first
+
+      FileUtils.rm_rf("#{path}/tmp")
+      FileUtils.mkdir_p("#{path}/tmp/migrate")
+
+      migrates_app = Dir.glob(app.config.paths["db/migrate"].first + "/*")
+      migrates_framework = Dir.glob(config.paths["db/migrate"].first + "/*")
+
+      migrates_framework.each do |m|
+        migrate = m.to_s.scan(/([a-zA-Z_]+[^rb])/).last.first
+        unless migrates_app.any? { |m_app| m_app.include?(migrate) }
+          result = m.split("/").last
+          FileUtils.cp(m, "#{path}/tmp/migrate/#{result}")
+        end
       end
+      
+      app.config.paths["db/migrate"] << "#{path}/tmp/migrate"
     end
 
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
-      # g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
       g.assets false
       g.helper false
     end
