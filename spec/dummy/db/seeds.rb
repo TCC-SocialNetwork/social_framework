c21ac45856dd56586cad12dac4a25838f74cdc51
Álex Mesquita <alex.mesquita06@hotmail.com>
2016-05-16 10:52:58 -0300

    Updating seeds
    Signed-off-by: Álex Mesquita <alex.mesquita06@hotmail.com>
    Signed-off-by: Jefferson Xavier <jeffersonx.xavier@gmail.com>

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,51 +1,55 @@
-# r = Random.new
+MAX_USER = 1000000
+QTD_RELATIONSHIPS = 400
+QTD_EVENTS = 10
 
-# (1..1000).each do |i|
-#   puts "#{i}"
-#   SocialFramework::User.create username: "user#{i}", email: "user#{i}@email.com", password: "12345678"
-# end
-# puts "Usuários Cadastrados"
+r = Random.new
 
-# (1..499).each do |i|
-#   puts "Usuário #{i}"
-#   u1 = SocialFramework::User.find i
-#   result = (i/100)
+(1..MAX_USER).each do |i|
+  puts "#{i}"
+  SocialFramework::User.create username: "user#{i}", email: "user#{i}@email.com", password: "12345678"
+end
+puts "Usuários Cadastrados"
 
-#   ((result*100+500)..(result*100+600)).each do |j|
-#     u2 = SocialFramework::User.find j
-#     u1.create_relationship u2, "friend", true, true
-#   end
+(1..((MAX_USER/2)-1)).each do |i|
+  puts "Usuário #{i}"
+  u1 = SocialFramework::User.find i
+  result = (i/QTD_RELATIONSHIPS)
 
-#   puts "arestas criados"
+  ((result*QTD_RELATIONSHIPS+MAX_USER/2)..(result*QTD_RELATIONSHIPS+(MAX_USER/2+QTD_RELATIONSHIPS))).each do |j|
+    u2 = SocialFramework::User.find j
+    u1.create_relationship u2, "friend", true, true
+  end
 
-#   (1..5).each do |k|
-#     result = nil
+  puts "arestas criados"
 
-#     while (result == nil)
-#       puts "evento #{k}"
-#       day = r.rand(1..28)
-#       month = r.rand(1..12)
-#       hour = r.rand(0..23)
-#       duration = r.rand(1..4)
-#       date = DateTime.new(2016, month, day, hour, 0, 0)
+  (1..QTD_EVENTS).each do |k|
+    result = nil
 
-#       result = u1.schedule.create_event "Evento #{k} do user#{i}", date, duration.hour
-#     end
-#   end
-# end
-# puts "Eventos e relacionamentos Cadastrados"
+    while (result == nil)
+      puts "evento #{k}"
+      day = r.rand(1..28)
+      month = r.rand(1..12)
+      hour = r.rand(0..23)
+      duration = r.rand(1..4)
+      date = DateTime.new(2016, month, day, hour, 0, 0)
 
-# (500..1000).each do |i|
-#   puts "Usuário #{i}"
-#   u1 = SocialFramework::User.find i
+      result = u1.schedule.create_event "Evento #{k} do user#{i}", date, duration.hour
+    end
+  end
+end
+puts "Eventos e relacionamentos Cadastrados"
+
+((MAX_USER/2)..MAX_USER).each do |i|
+  puts "Usuário #{i}"
+  u1 = SocialFramework::User.find i
   
-#   (1..5).each do |k|
-#     result = nil
+  (1..QTD_EVENTS).each do |k|
+    result = nil
 
-#     while result == nil
-#       id = r.rand(1..2495)
-#       e = SocialFramework::Event.find id
-#       result = u1.schedule.enter_in_event e
-#     end
-#   end
-# end
+    while result == nil
+      id = r.rand(1..(((MAX_USER/2)-1)*QTD_EVENTS))
+      e = SocialFramework::Event.find id
+      result = u1.schedule.enter_in_event e
+    end
+  end
+end
